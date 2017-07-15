6e46a94b30cfcf6ce082e65551dd67c4b2b4e1a8
HienNguyen711 <hienminhnguyen711>
2017-07-15 23:10:26 +0300

    project add

---
+++ my_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/nguyenvantai/Desktop/Ex_Files_Django_1_Blog/project/my_env/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
